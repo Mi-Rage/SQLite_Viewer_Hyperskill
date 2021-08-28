432d59c269e5a88c00f392700db786d70cd9bbf5
Mi-Rage <59646864+Mi-Rage@users.noreply.github.com>
2021-08-28 15:18:47 +0300

    SQLite Viewer Stage 2/4
    SQLite Viewer JAVA project. Stage 2/4 complete.

--- viewer/SQLiteViewer.java
+++ viewer/SQLiteViewer.java
@@ -1,11 +1,14 @@
 package viewer;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class SQLiteViewer extends JFrame {
 
     final String TITLE_OF_PROGRAM = "SQLite Viewer";
-    final String BTN_ENTER = "Enter";
+    final String BTN_ENTER = "Open";
+    final String BTN_EXEC = "Execute";
+    DataBase dataBase;
 
     public SQLiteViewer() {
         setTitle(TITLE_OF_PROGRAM);
@@ -17,14 +20,47 @@ public class SQLiteViewer extends JFrame {
 
         JTextField fileNameTextField = new JTextField();
         fileNameTextField.setName("FileNameTextField");
-        fileNameTextField.setBounds(20,20, getWidth() - 130,30);
+        fileNameTextField.setBounds(20, 20, getWidth() - 130, 30);
         add(fileNameTextField);
 
-        fileNameTextField.setName("FileNameTextField");
         JButton openFileButton = new JButton(BTN_ENTER);
         openFileButton.setName("OpenFileButton");
         openFileButton.setBounds(getWidth() - 100, 20, 70, 30);
         add(openFileButton);
+
+        JComboBox<String> tablesComboBox = new JComboBox<>();
+        tablesComboBox.setName("TablesComboBox");
+        tablesComboBox.setBounds(20, 70, getWidth() - 50, 30);
+        add(tablesComboBox);
+
+        JTextArea queryTextArea = new JTextArea();
+        queryTextArea.setName("QueryTextArea");
+        queryTextArea.setBounds(20, 120, getWidth() - 170, 60);
+        add(queryTextArea);
+
+        JButton executeQueryButton = new JButton(BTN_EXEC);
+        executeQueryButton.setName("ExecuteQueryButton");
+        executeQueryButton.setBounds(getWidth() - 140, 120, 110, 30);
+        add(executeQueryButton);
+
+        openFileButton.addActionListener(e -> {
+            String fileName = fileNameTextField.getText();
+            if (!fileName.equals("")) {
+                System.out.println(fileName);
+                dataBase = new DataBase(fileName);
+                ArrayList<String> tables = dataBase.getTableNames();
+                tablesComboBox.removeAllItems();
+                tables.forEach(tablesComboBox::addItem);
+                queryTextArea.removeAll();
+            }
+        });
+
+        tablesComboBox.addActionListener(e -> {
+            String item = (String) tablesComboBox.getSelectedItem();
+            queryTextArea.removeAll();
+            queryTextArea.setText("SELECT * FROM " + item + ";");
+        });
+
         setVisible(true);
     }
 }
